6a33c84ede65d98b289da9a9932b3cea779a8381
stevengogogo <stevengogogo4321@gmail.com>
2021-05-08 14:10:10 +0800

    Draft: Add packdata

--- src/list.h
+++ src/list.h
@@ -24,33 +24,50 @@ typedef struct hnode{
 
 /** * Production line*/
 typedef struct{
-    List* list;
+    List list;
     hnode* heap;
     int avail;
 } prodLine;
 
+typedef struct{
+    int N_Package;
+    int N_Lines;
+    prodLine* lines;
+    pack* packArr;
+} packData;
 
 // Initiation and Deletion
-pack* init_packArr(int n);
-void kil_packArr(pack*);
 
-// Manage Data
-pack PushPack(prodLine);
-pack MergeLines(prodLine dst, prodLine src);
-pack PopFirstPack(prodLine);
-pack PopLastPack(prodLine);
-pack PopMaxPack(prodLine);
+/**
+ * @brief Initiate a package management problem
+ * 
+ * @param n number of packages
+ * @param l number of lines
+ * @return packData 
+ */
+packData init_Problem(int n, int l);
+void kill_Problem(packData);
+
+// Data management
+pack PushPack(packData, int i);
+pack MergeLines(packData, int dst, int src);
+
+//Pop
+pack PopFirstPack(packData, int i);
+pack PopLastPack(packData, int i);
+pack PopMaxPack(packData, int i);
 
 // Peak Data
-pack PeakFirstPack(prodLine);
-pack PeakLastPack(prodLine);
-pack PeakMaxPack(prodLine);
+pack PeakFirstPack(packData, int i);
+pack PeakLastPack(packData, int i);
+pack PeakMaxPack(packData, int i);
 
 
 // Heap operation
 void _insertHeap(hnode* root);
 void _mergeHeap(hnode* rootDst, hnode* rootSrc);
 void _extractMaxHeap(hnode* root);
+void _killHeap(hnode* root);
 
 //Linked operation
 void _insertlist(List);
