c9fdbe6c72ae3714d1ba15da6893fe1ee72c4f3e
stevengogogo <stevengogogo4321@gmail.com>
2021-05-08 16:44:50 +0800

    init docstring: main

--- src/main.c
+++ src/main.c
@@ -1,11 +1,23 @@
+/**
+ * @file main.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-05-08
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdio.h>
+
 #include "utils.h"
+#include "list.h"
 
 int main()
 {
-    
+
 
     return 0;
 }
